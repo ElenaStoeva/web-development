ea2dbd5d80df47f052ebdd02c55af58b11322b6f
Elena Svetoslavova Stoeva <ess243@cornell.edu>
2022-03-30 23:32:24 +0000

    Copy Project 2 site files into this project repository

--- pages/404.php
+++ pages/404.php
@@ -4,13 +4,15 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-  <title>TODO: 404</title>
+  <link rel="stylesheet" href="public/styles/site.css">
+  <title>Not Found</title>
 </head>
 
 <body>
 
-  TODO: 404
+  <h1>Playful Plants Project</h1>
+  <p>The page that you are looking for does not exist.</p>
+  <p>Our Plant Catalog is <a href="/">here</a>.</p>
 
 </body>
 
