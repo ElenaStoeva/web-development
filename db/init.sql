83cc0a3919db5560e6443c23c272d4066f2c5ba3
Elena Svetoslavova Stoeva <ess243@cornell.edu>
2022-04-10 04:16:06 +0000

    Delete Project 2 database and initialize database with SQL script

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,165 @@
--- TODO: create tables
+--- Plants Table ---
 
--- CREATE TABLE `examples` (
--- 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
--- 	`name`	TEXT NOT NULL
--- );
+CREATE TABLE plants (
+	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	plant_name_coll TEXT NOT NULL,
+  plant_name_spec TEXT NOT NULL UNIQUE,
+  plant_ID TEXT NOT NULL UNIQUE,
+  exploratory_constructive_play INTEGER NOT NULL,
+  exploratory_sensory_play INTEGER NOT NULL,
+  physical_play INTEGER NOT NULL,
+  imaginative_play INTEGER NOT NULL,
+  restorative_play INTEGER NOT NULL,
+  expressive_play INTEGER NOT NULL,
+  play_with_rules INTEGER NOT NULL,
+  bio_play INTEGER NOT NULL
+);
+
+--- Initial plant records ---
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (1, '3 Sisters-Corn', 'Red Mohawk Corn', 'FE_07', 0, 1, 1, 1, 1, 0, 1, 0);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (2, 'American Groundnut', 'Apius americana', 'VI_05', 0, 1, 1, 1, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (3, 'Common Nasturtiums', 'Tropaeolum (group)', 'VI_01', 0, 1, 0, 1, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (4, 'Downy skullcap', 'Scutellaria incana', 'FL_28', 0, 1, 0, 1, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (5, 'White Turtlehead', 'Chelone glabra', 'FL_10', 0, 1, 0, 1, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (6, 'Blue Grama Grass', 'Bouteloua gracilis', 'GA_12', 1, 1, 1, 0, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (7, 'Feather Moss', 'Hypnum imponens', 'FE_16', 0, 1, 0, 1, 1, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (8, 'Cattails', 'Typha latifolia', 'GA_14-W', 1, 1, 1, 1, 1, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (9, 'American Persimmon', 'Diospyros virginiana', 'TR_14', 1, 1, 1, 0, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (10, 'Northern BayberryÂ ', 'Myrica pensylvanica', 'SH_04', 0, 1, 1, 0, 1, 0, 1, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (11, 'Russian Sage', 'Perovskia atriplicifolia', 'FL_18', 0, 1, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (12, 'Chinese Chestnuts', 'Castanea mollissima', 'TR_16', 1, 1, 1, 1, 1, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (13, 'Cranesbill', 'Geranium maculatum', 'FL_04', 0, 1, 0, 0, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (14, 'Jack-in-the-Pulpit', 'Arisaema triphyllum', 'FL_36', 0, 1, 1, 1, 0, 0, 0, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (15, 'Dappled WillowÂ', "Salix integra 'Hakuro Nishiki", 'SH_08', 0, 1, 1, 1, 1, 0, 1, 1);
+
+INSERT INTO
+  plants (id, plant_name_coll, plant_name_spec, plant_ID, exploratory_constructive_play, exploratory_sensory_play, physical_play, imaginative_play, restorative_play, expressive_play, play_with_rules, bio_play)
+VALUES
+  (16, 'Sensitive Fern', 'Onodea sensibilis', 'FE_03', 0, 1, 0, 0, 1, 0, 0, 0);
 
 
--- TODO: initial seed data
+--- Tags Table ---
 
--- INSERT INTO `examples` (name) VALUES ('example-1');
--- INSERT INTO `examples` (name) VALUES ('example-2');
+CREATE TABLE tags (
+	tag_id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	tag_name TEXT NOT NULL UNIQUE
+);
 
+--- Initial tag records ---
+
+INSERT INTO
+  tags (tag_id, tag_name)
+VALUES
+  (1, 'Red');
+
+INSERT INTO
+  tags (tag_id, tag_name)
+VALUES
+  (2, 'Orange');
+
+INSERT INTO
+  tags (tag_id, tag_name)
+VALUES
+  (3, 'Yellow');
+
+
+--- Tagging Table ---
+
+CREATE TABLE taggings (
+	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  plant_id TEXT NOT NULL PRIMARY KEY,
+	tag_id TEXT NOT NULL PRIMARY KEY
+);
+
+--- Initial tagging records ---
+
+INSERT INTO
+  taggings (id, plant_id, tag_id)
+VALUES
+  (1, 1, 1);
+
+INSERT INTO
+  taggings (id, plant_id, tag_id)
+VALUES
+  (2, 2, 3);
+
+INSERT INTO
+  taggings (id, plant_id, tag_id)
+VALUES
+  (3, 3, 4);
+
+
+--- Users Table ---
+
+CREATE TABLE users (
+	user_id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL UNIQUE,
+);
+
+--- Initial user records ---
+
+INSERT INTO
+  users (user_id, username, password)
+VALUES
+  (1, 'admin', 'admin');
