cdce6e6f30171230a08ec7eea664486292c7ff6a
Elena Svetoslavova Stoeva <ess243@cornell.edu>
2022-04-14 02:51:15 +0000

    Implement live view all and details pages

--- pages/details.php
+++ pages/details.php
@@ -1,3 +1,15 @@
+<?php
+$db = init_sqlite_db('db/site.sqlite', 'db/init.sql');
+$plant_ID = $_GET['id'];
+$sql_query = "SELECT * FROM plants WHERE plant_ID ='" . $plant_ID . "'";
+$records = exec_sql_query($db, $sql_query)->fetchAll();
+$record = $records[0];
+$file_name = "./public/photos/" . $plant_ID . ".jpg";
+if (!file_exists($file_name)) {
+  $file_name = "/public/photos/image_placeholder.jpg";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,31 +25,58 @@
   <h1>Playful Plants Project</h1>
 
   <div class="tile">
-    <img src="/public/photos/SH_11_1.jpg" alt="Plant">
+    <img src=<?php echo htmlspecialchars($file_name); ?> alt="Plant Image" width="400">
     <div class="tile-header">
-      <h3>Black cohosh</h3>
-      <h4>Actaea racemosa</h4>
-      <h5>Plant ID: FL_24</h5>
+      <h3><?php echo htmlspecialchars($record['plant_name_coll']); ?></h3>
+      <h4><?php echo htmlspecialchars($record['plant_name_spec']); ?></h4>
+      <h5>Plant ID: <?php echo htmlspecialchars($plant_ID); ?></h5>
     </div>
     <ul>
-      <li>
-        Supports Exploratory Sensory Play
-      </li>
-
-      <li>
-        Supports Physical Play
-      </li>
-      <li>
-        Supports Imaginative Play
-      </li>
-
-
-      <li>
-        Supports Bio Play
-      </li>
+      <?php if ($record['exploratory_constructive_play']) { ?>
+        <li>
+          Supports Exploratory Constructive Play
+        </li>
+      <?php } ?>
+      <?php if ($record['exploratory_sensory_play']) { ?>
+        <li>
+          Supports Exploratory Sensory Play
+        </li>
+      <?php } ?>
+      <?php if ($record['physical_play']) { ?>
+        <li>
+          Supports Physical Play
+        </li>
+      <?php } ?>
+      <?php if ($record['imaginative_play']) { ?>
+        <li>
+          Supports Imaginative Play
+        </li>
+      <?php } ?>
+      <?php if ($record['restorative_play']) { ?>
+        <li>
+          Supports Restorative Play
+        </li>
+      <?php } ?>
+      <?php if ($record['expressive_play']) { ?>
+        <li>
+          Supports Expressive Play
+        </li>
+      <?php } ?>
+      <?php if ($record['play_with_rules']) { ?>
+        <li>
+          Supports Play with Rules
+        </li>
+      <?php } ?>
+      <?php if ($record['bio_play']) { ?>
+        <li>
+          Supports Bio Play
+        </li>
+      <?php } ?>
 
     </ul>
   </div>
+
+  <a href="/">Go back to catalog</a>
 
 </body>
 
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,14 @@
+<?php
+
+$db = init_sqlite_db('db/site.sqlite', 'db/init.sql');
+
+// This query will be changed for future implementations.
+$sql_query = 'SELECT * FROM plants';
+$records = exec_sql_query($db, $sql_query)->fetchAll();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,84 +23,43 @@
 
   <h1>Playful Plants Project</h1>
 
-  <a href="/login">
-    <button>Log In</button>
-  </a>
-
   <div class="row">
-    <div>
-      <div class="sort-filter">
-        Sort:
-        <select name="sort">
-          <option value="Name Ascending">Name Ascending</option>
-          <option value="Name Descending">Name Descending</option>
-        </select>
-      </div>
-      <div class="sort-filter">
-        <button>Filter</button>
-      </div>
+    <div class="login">
+      <a href="/login">
+        <button>Log In</button>
+      </a>
     </div>
-
-
-    <div>
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 1</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 2</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 3</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 4</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 5</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 6</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 7</div>
-        </div>
-      </div>
-
-      <div class="gallery">
-        <a href="/details"><img src="/public/photos/SH_11_1.jpg" alt="Plant" width="200" height="200"></a>
-        <div class="tile-header">
-          <div class="name">Plant 8</div>
-        </div>
-      </div>
-
+    <div class="sort-filter">
+      Sort:
+      <select name="sort">
+        <option value="Name Ascending">Name Ascending</option>
+        <option value="Name Descending">Name Descending</option>
+      </select>
+    </div>
+    <div class="sort-filter">
+      <button>Filter</button>
     </div>
   </div>
+
+  <div class="catalog-gallery">
+    <?php
+    foreach ($records as $record) {
+      $plant_ID = $record['plant_ID'];
+      $file_name = "./public/photos/" . $plant_ID . ".jpg";
+      if (!file_exists($file_name)) {
+        $file_name = "/public/photos/image_placeholder.jpg";
+      }
+    ?>
+      <div class="gallery">
+        <a href="/details?<?php echo http_build_query(array('id' => $plant_ID)); ?>"><img src=<?php echo htmlspecialchars($file_name); ?> alt="Plant" width="200" height="200"></a>
+        <div class="tile-header">
+          <div class="name"><?php echo htmlspecialchars($record['plant_name_coll']); ?></div>
+        </div>
+      </div>
+    <?php } ?>
+
+  </div>
+
 
 
 </body>
